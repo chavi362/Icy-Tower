750e3e4e4403ddbbd2623689853154dbf80a3f7d
Johnny Cruz III <92472468+Jcruz525@users.noreply.github.com>
2024-01-05 20:24:52 -0500

    Fixing spacing in character select page

--- js/iframe-hello-user.js
+++ js/iframe-hello-user.js
@@ -1,8 +1,6 @@
 function updateHeloUser() {
   currentUser = JSON.parse(sessionStorage.getItem('currentUser'));
   let spanFName = document.getElementById('user-first-name');
-  spanFName.innerHTML = 'hello ' + currentUser.firstName;
-  let spanUserName = document.getElementById('user-name');
-  spanUserName.innerHTML = currentUser.userName;
+  spanFName.innerHTML = 'Hello,' + currentUser.firstName;
 }
 updateHeloUser();
